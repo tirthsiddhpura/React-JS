449feba35bc08beef9c46a99129a411469bcbfbf
tirthsiddhpura <tirthsiddhpura543@gmail.com>
2026-03-16 17:48:13 +0530

    first commit

--- keep-note/src/App.jsx
+++ keep-note/src/App.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+   <>
+   {/* form for add new task*/}
+   <section className='lg:flex items-center justify-center'>
+    <div className='lg:w-1/2 w-full h-screen'>
+    <form>
+      <h1></h1>
+    </form>
+    </div>
+    {/* div for show all task*/}
+    <div className='lg:w-1/2 w-full  h-screen'></div>
+   </section>
+   </>
   )
 }
 
